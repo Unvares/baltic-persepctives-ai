9db0b6ad92bce222c10cbfead68e8822eb3ac99a
Nane Kratzke <nane.kratzke@th-luebeck.de>
2024-07-05 22:16:01 +0200

    secret handling

--- chatbot-rag/chatbot.py
+++ chatbot-rag/chatbot.py
@@ -3,36 +3,27 @@ import streamlit as st
 from openai import OpenAI
 from langchain_openai import OpenAIEmbeddings
 from langchain_chroma import Chroma
-from langchain_core.documents import Document
 
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY", "-")
 LLM_ENDPOINT = os.environ.get("LLM_ENDPOINT", "https://chat-large.llm.mylab.th-luebeck.dev/v1")
-LLM_PREPROCESS_ENDPOINT = os.environ.get("LLM_ENDPOINT", "https://chat-slim.llm.mylab.th-luebeck.dev/v1")
 
-EMBEDDING_ENDPOINT = os.environ.get("EMBEDDING_ENDPOINT", "")
-
-if EMBEDDING_ENDPOINT:
-    embeddings = OpenAIEmbeddings(base_url=LLM_ENDPOINT, api_key="-")
-else:
-    embeddings = OpenAIEmbeddings(api_key="sk-proj-M0N3AniQmJwCGttxwUn5T3BlbkFJ6VhsTuPbdauF8WeMrxRv")
-
+embeddings = OpenAIEmbeddings(api_key=OPENAI_API_KEY)
 llm = OpenAI(base_url=LLM_ENDPOINT, api_key=OPENAI_API_KEY)
-preprocess_llm = OpenAI(base_url=LLM_PREPROCESS_ENDPOINT, api_key=OPENAI_API_KEY)
-
 vectorstore = Chroma(embedding_function=embeddings, persist_directory="/data")
-
 
 st.set_page_config(page_title="KIRA", page_icon="🦜", menu_items={})
 st.title("🦜 Ask me anything ...")
 st.markdown("## ... about prompt engineering")
 
-st.session_state['messages'] = st.session_state.get('messages', [{
+system_prompt = [{
     "role": "system", 
-    "content": "Your name is KIRA and you are a prompt engineering expert. If you have references, you link them in your responses."
-}])
+    "content": "Your name is KIRA and you are a prompt engineering expert. Link references in your responses as URLs."
+}]
+
+st.session_state['messages'] = st.session_state.get('messages', [])
 
 # Show previous messages
-for entry in st.session_state['messages'][1:]:
+for entry in st.session_state['messages']:
     with st.chat_message(entry['role']):
         st.write(entry['content'])
 
@@ -44,13 +35,14 @@ if prompt := st.chat_input("🦜 Ask me anything about prompt engineering ..."):
     with st.chat_message("ai"):
         with st.status("Querying knowledge base"):
             retriever = vectorstore.as_retriever(search_kwargs={'k': 5})
-            last_response = st.session_state.messages[-1]['content']
+            last_response = st.session_state.messages[-1]['content'] if st.session_state.messages else ""
             ctx = '\n\n'.join(c.page_content for c in retriever.invoke(last_response + "\n" + prompt))
             st.write(f"Found a context with {len(ctx)} characters.")
 
+        last_messages = st.session_state.messages[-2:] if len(st.session_state.messages) > 2 else st.session_state.messages
         completion = llm.chat.completions.create(
-            messages=st.session_state.messages + [
-                { "role": "system", "content": f"Consider this context and references from your knowledge base in your answer. Context:\n\n{ctx}" },
+            messages=system_prompt + last_messages + [
+                { "role": "system", "content": f"Consider your knowledge base. Context:\n\n{ctx}" },
                 { "role": "user", "content": prompt }
             ],
             model="", stream=True, max_tokens=4000
