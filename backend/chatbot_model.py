a5124a94a58bcf2c001f6b8ad5d667cdebce7c74
Justifull <felix.potratz2003@gmail.com>
2024-07-20 20:39:28 +0200

    Added max tokens and fixed a bug where providing a topic without content of the same topic it chooses the wrong or none topic.

--- backend/chatbot_model.py
+++ backend/chatbot_model.py
@@ -4,6 +4,7 @@ from langchain_openai import ChatOpenAI
 
 
 def get_model():
-    api_key = os.environ.get("API_KEY", "-")
-    model = ChatOpenAI(streaming=True, api_key=api_key, model="gpt-4o-mini")
+    api_key = "sk-proj-AdIuNRsaRBF521qXqYoST3BlbkFJBxgJ7H7mTT8u3uN9xmG5"
+    # api_key = os.environ.get("API_KEY", "-")
+    model = ChatOpenAI(streaming=True, api_key=api_key, model="gpt-4o-mini", max_tokens=100)
     return model
--- backend/topic_chain.py
+++ backend/topic_chain.py
@@ -6,31 +6,33 @@ from chatbot_model import get_model
 from personas import PERSONAS
 
 
-def get_persona_chain(question):
-    model = get_model()
-    persona_chain = (PromptTemplate.from_template(
-        """Given the user question below, detect the country that the content of the question has the biggest relation to as its english name.
-                        Differentiate between the countries 'sweden', 'denmark', 'lithuania', 'iceland', 'norway', 'latvia',
-                        'finland', 'poland', 'germany', 'estonia'. If you cant find no matches use 'poland' as the value.
+def get_persona_chain(topic, question):
+    if topic == "group":
+        model = get_model()
+        persona_chain = (PromptTemplate.from_template(
+            """Given the user question below, detect the country that the content of the question has the biggest relation to as its english name.
+                            Differentiate between the countries 'sweden', 'denmark', 'lithuania', 'iceland', 'norway', 'latvia',
+                            'finland', 'poland', 'germany', 'estonia'. If you cant find no matches use 'poland' as the value.
 
-                        Do not respond with more than one word or uppercase letters.
+                            Do not respond with more than one word or uppercase letters.
 
-                        <question>
-                        {question}
-                        </question>
+                            <question>
+                            {question}
+                            </question>
 
-                        Classification:"""
-    )
-                     | model
-                     | StrOutputParser()
-                     )
-    answer = persona_chain.invoke({'question': question})
-    return answer
+                            Classification:"""
+        )
+                         | model
+                         | StrOutputParser()
+                         )
+        answer = persona_chain.invoke({'question': question})
+        return answer
+    return topic
 
 
 def create_persona_chain(country, question):
     if country == "group":
-        generated_country = get_persona_chain(question)[:3].lower()
+        generated_country = get_persona_chain(country, question)[:3].lower()
         prompt_character = ChatPromptTemplate.from_messages([
             ('system', f"You are the following person: {PERSONAS[generated_country]}")
         ])
