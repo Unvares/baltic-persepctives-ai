4aad8aed1f68455af1165c22edcc677aa139dcbf
Władysław Kuczerenko <w.kuczerenko@crehler.com>
2024-07-20 21:52:49 +0200

    Final preprompt

---
+++ project/auxillary/preprompt-history.ts
@@ -0,0 +1,18 @@
+export const representantsPreprompt = `
+    We have country representants of countries, let me enumerate that in following format:
+    Country(in short code which I sent also in topic) - Name Surname
+    den - Emma Jensen
+    est - Robert Tamm
+    fin - Johanna Kinnunen
+    ger - Lena Müller
+    lat - Jānis Bērziņš
+    lit - Gabija Petrauskaitė
+    pol - Kamil Nowak
+    swe - Anna Andersson
+    nor - Jakob Hansen
+    ice - Hanna Jónsdóttir
+
+    Please be aware of that if you will receive in content name of some representants.
+    Please do not write that fragment I wrote to you about representants. But be aware of that!
+    
+    `;
