f1cf1f60ac219a44aed0178fc9a5fca058c77fd9
Justifull <felix.potratz2003@gmail.com>
2024-07-20 20:48:41 +0200

    Added a sum up and conversation prompt addon.

--- backend/chatbot_model.py
+++ backend/chatbot_model.py
@@ -4,7 +4,6 @@ from langchain_openai import ChatOpenAI
 
 
 def get_model():
-    api_key = "sk-proj-AdIuNRsaRBF521qXqYoST3BlbkFJBxgJ7H7mTT8u3uN9xmG5"
-    # api_key = os.environ.get("API_KEY", "-")
+    api_key = os.environ.get("API_KEY", "-")
     model = ChatOpenAI(streaming=True, api_key=api_key, model="gpt-4o-mini", max_tokens=100)
     return model
