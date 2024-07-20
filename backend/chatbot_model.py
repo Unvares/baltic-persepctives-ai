73e955fd49b9c7df96abd1ce6265cba016582839
Justifull <felix.potratz2003@gmail.com>
2024-07-20 21:44:48 +0200

    Removed max_tokens.

--- backend/chatbot_model.py
+++ backend/chatbot_model.py
@@ -6,6 +6,6 @@ from langchain_openai import ChatOpenAI
 def get_model():
     api_key = os.environ.get("API_KEY", "-")
     model = ChatOpenAI(
-        streaming=True, api_key=api_key, model="gpt-4o-mini", max_tokens=100
+        streaming=True, api_key=api_key, model="gpt-4o-mini"
     )
     return model
